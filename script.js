3e44bc30eac408424d94adbd26a4c569372c2722
Bogdan <bogdan.kazarean@gmail.com>
2022-11-02 18:20:58 +0400

    ad

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function(){
+$(".divhead1").on("click",function(){
+
+$(".divbody1").slideToggle(2000);
+$(".div1").toggleClass("active");
+
+
+})
+$(".divhead2").on("click",function(){
+
+    $(".divbody2").slideToggle(2000);
+    $(".div2").toggleClass("active");
+    
+    
+    })
+    $(".divhead3").on("click",function(){
+
+        $(".divbody3").slideToggle(2000);
+        $(".div3").toggleClass("active");
+        
+        })
+
+
+
+})
